63eaf237092d3a204f50b1f32b726f3834335d81
Tyler Waters <tyler.waters@epicureselections.com>
2016-01-11 13:51:40 -0800

    Better sniffing of content type.
    In the current version of express, the charset is appended to the content
    type. Update the sniffing to check for the presence of `application/json`
    and not the exact string.
    
    Fixes #1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (express) {
   express.response.send = function(code, body) {
     body == null && (body=code, code=null)
 
-    if (this.get('Content-Type') === 'application/json' && typeof body === 'string')
+    if ((this.get('Content-Type') || "").indexOf('application/json') > -1 && typeof body === 'string')
       body = body.replace(/</g, '\\u003c')
 
     return code
