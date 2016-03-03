7d3adab2363c4371f08aa93f2ce20fdd34ea5d4f
Nadav Ivgi <git@shesek.info>
2016-03-03 14:17:45 +0200

    add test for non-json responses

--- test.js
+++ test.js
@@ -4,16 +4,25 @@ var safe_json = require('./index')
   , request   = require('supertest')
 
 describe('express-safe-json', function() {
+  var app = express()
+  app.set('json spaces', false)
+  safe_json(app)
+
   it('escapes HTML in JSON responses', function(done) {
-    var app = express()
-    app.set('json spaces', false)
-    safe_json(app)
-    app.get('/', function(req, res) { res.json({'<foo>':'<bar>'}) })
+    app.get('/json', function(req, res) { res.json({'<foo>':'<bar>'}) })
 
     request(app)
-      .get('/')
+      .get('/json')
       .expect('Content-Type', /json/)
       .expect('{"\\u003cfoo>":"\\u003cbar>"}')
       .expect({'<foo>':'<bar>'}, done)
+  })
+
+  it('should not modify non-JSON responses', function(done) {
+    app.get('/text', function(req, res) { res.send('<foo><bar>') })
+
+    request(app)
+      .get('/text')
+      .expect('<foo><bar>', done)
   })
 })
